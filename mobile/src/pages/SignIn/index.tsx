b65dcb26c55711847144a3cf45eee3a1b8487f9e
Muller Esposito <mulleresposito@hotmail.com>
2021-01-30 02:21:15 -0300

    *

--- mobile/src/pages/SignIn/index.tsx
+++ mobile/src/pages/SignIn/index.tsx
@@ -1,9 +1,66 @@
-import React from 'react';
+import React, { useCallback, useRef } from 'react';
+import { Image, KeyboardAvoidingView, ScrollView } from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
+import { useNavigation } from '@react-navigation/native';
+import { Form } from '@unform/mobile';
+import { FormHandles } from '@unform/core';
 
-import { Container } from './styles';
+import logoImg from '../../assets/logo.png';
+
+import Button from '../../components/Button';
+import Input from '../../components/Input';
+
+import {
+  Container,
+  Title,
+  ForgotPassword,
+  TextForgotPassword,
+  CreateAccountButton,
+  TextCreateAccountButton,
+} from './styles';
 
 const SignIn: React.FC = () => {
-  return <Container />;
+  const formRef = useRef<FormHandles>(null);
+  const navigation = useNavigation();
+
+  const handleSignIn = useCallback(data => {
+    console.log(data);
+  }, []);
+
+  return (
+    <>
+      <KeyboardAvoidingView style={{ flex: 1 }} enabled>
+        <ScrollView
+          contentContainerStyle={{ flex: 1 }}
+          keyboardShouldPersistTaps="handled"
+        >
+          <Container>
+            <Image source={logoImg} />
+
+            <Title>Faça seu logon</Title>
+
+            <Form ref={formRef} onSubmit={handleSignIn}>
+              <Input name="email" icon="mail" placeholder="E-mail" />
+              <Input name="password" icon="lock" placeholder="Senha" />
+
+              <Button onPress={() => formRef.current?.submitForm()}>
+                Entrar
+              </Button>
+            </Form>
+
+            <ForgotPassword>
+              <TextForgotPassword>Esqueci minha senha</TextForgotPassword>
+            </ForgotPassword>
+          </Container>
+        </ScrollView>
+      </KeyboardAvoidingView>
+
+      <CreateAccountButton onPress={() => navigation.navigate('SignUp')}>
+        <Icon name="log-in" size={24} color="#ff9000" />
+        <TextCreateAccountButton>Criar conta</TextCreateAccountButton>
+      </CreateAccountButton>
+    </>
+  );
 };
 
 export default SignIn;
--- mobile/src/pages/SignUp/index.tsx
+++ mobile/src/pages/SignUp/index.tsx
@@ -1,9 +1,42 @@
 import React from 'react';
+import { Image, KeyboardAvoidingView } from 'react-native';
+import { ScrollView } from 'react-native-gesture-handler';
+import Icon from 'react-native-vector-icons/Feather';
+import { useNavigation } from '@react-navigation/native';
 
-import { Container } from './styles';
+import logoImg from '../../assets/logo.png';
+import Button from '../../components/Button';
+import Input from '../../components/Input';
+
+import { Container, Title, BackToSignIn, TextBackToSignIn } from './styles';
 
 const SignUp: React.FC = () => {
-  return <Container />;
+  const navigation = useNavigation();
+
+  return (
+    <>
+      <KeyboardAvoidingView style={{ flex: 1 }}>
+        <ScrollView contentContainerStyle={{ flex: 1 }}>
+          <Container>
+            <Image source={logoImg} />
+
+            <Title>Crie sua conta</Title>
+
+            <Input name="user" icon="user" placeholder="Nome" />
+            <Input name="email" icon="mail" placeholder="E-mail" />
+            <Input name="password" icon="lock" placeholder="Senha" />
+
+            <Button>Entrar</Button>
+          </Container>
+        </ScrollView>
+      </KeyboardAvoidingView>
+
+      <BackToSignIn onPress={() => navigation.goBack()}>
+        <Icon name="arrow-left" size={24} color="#f4ede8" />
+        <TextBackToSignIn>Voltar para o logon</TextBackToSignIn>
+      </BackToSignIn>
+    </>
+  );
 };
 
 export default SignUp;
