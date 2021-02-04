bf78b31843b8c0256ed58001a22f771bfa1a40ab
Muller Esposito <mulleresposito@hotmail.com>
2021-02-03 23:49:01 -0300

    There was imported the CreateAppointmentDTO interface to define the data format from data received by create method.

--- back-end/src/modules/appointments/infra/repositories/IAppointmentsRepository.ts
+++ back-end/src/modules/appointments/infra/repositories/IAppointmentsRepository.ts
@@ -1,6 +1,8 @@
+import ICreateAppointmentsDTO from '@modules/appointments/dto/ICreateAppointmentsDTO';
 import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment';
 
 interface IAppointmentsRepository {
+    create(data: ICreateAppointmentsDTO): Promise<Appointment>;
     findByDate(date: Date): Promise<Appointment | undefined>;
 }
 
