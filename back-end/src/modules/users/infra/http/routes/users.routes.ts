db749838fdb003413f701335ebe73c1cff26d5b3
Muller Esposito <mulleresposito@hotmail.com>
2021-02-08 01:10:35 -0300

    The logic that was in this route has been migrated to UsersController.

--- back-end/src/modules/users/infra/http/routes/users.routes.ts
+++ back-end/src/modules/users/infra/http/routes/users.routes.ts
@@ -1,55 +1,24 @@
 import { Router } from 'express';
 import multer from 'multer';
 
-import CreateUsersService from '@modules/users/services/CreateUserService';
-import UsersRepository from '@modules/users/infra/typeorm/entities/repositories/UsersRepository';
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 import uploadConfig from '@config/upload';
-import UpdateUserAvatarService from '@modules/users/services/UpdateUserAvatarService';
+import UsersController from '../contollers/UsersController';
+import AvatarUserController from '../contollers/AvatarUserController';
 
 const usersRouter = Router();
 const upload = multer(uploadConfig);
 
-usersRouter.post('/', async (request, response) => {
-    const usersRepository = new UsersRepository();
+const usersController = new UsersController();
+const avatarUserController = new AvatarUserController();
 
-    try {
-        const { name, email, password } = request.body;
-
-        const createUser = new CreateUsersService(usersRepository);
-
-        const user = await createUser.execute({
-            name,
-            email,
-            password,
-        });
-
-        delete user.password;
-
-        return response.status(200).json(user);
-    } catch (err) {
-        return response.status(400).json({ error: err.message });
-    }
-});
+usersRouter.post('/', usersController.create);
 
 usersRouter.patch(
     '/avatar',
     ensureAuthenticated,
     upload.single('avatar'),
-    async (request, response) => {
-        const usersRepository = new UsersRepository();
-
-        const updateUserAvatar = new UpdateUserAvatarService(usersRepository);
-
-        const user = await updateUserAvatar.execute({
-            user_id: request.user.id,
-            avatarFilename: request.file.filename,
-        });
-
-        delete user.password;
-
-        return response.status(200).json(user);
-    },
+    avatarUserController.update,
 );
 
 export default usersRouter;
