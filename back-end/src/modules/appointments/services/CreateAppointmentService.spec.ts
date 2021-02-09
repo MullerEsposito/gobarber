765c9e16ebceb03f47b3b614be172d900049e09c
Muller Esposito <mulleresposito@hotmail.com>
2021-02-09 16:29:15 -0300

    There was created a test to CreateAppointmentService

---
+++ back-end/src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -0,0 +1,19 @@
+import AppointmentsRepositoryFake from '@modules/appointments/infra/repositories/fakes/AppointmentsRepositoryFake';
+import CreateAppointmentService from './CreateAppointmentService';
+
+describe('CreateAppointment', () => {
+    it('should be able to create a new appointment', async () => {
+        const fakeAppointmentRepository = new AppointmentsRepositoryFake();
+        const createAppointment = new CreateAppointmentService(
+            fakeAppointmentRepository,
+        );
+
+        const appointment = await createAppointment.execute({
+            date: new Date(),
+            provider_id: '123123',
+        });
+
+        expect(appointment).toHaveProperty('id');
+        expect(appointment.provider_id).toBe('123123');
+    });
+});
