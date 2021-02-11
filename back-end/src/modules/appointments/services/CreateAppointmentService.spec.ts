5d71c9bf09599bb9d8ce7a18db260d3b8dc5787c
Muller Esposito <mulleresposito@hotmail.com>
2021-02-10 21:58:18 -0300

    There was created a test to verify if the service rejects the create of two appointments on same date and hour.

--- back-end/src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ back-end/src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,4 +1,5 @@
 import AppointmentsRepositoryFake from '@modules/appointments/infra/repositories/fakes/AppointmentsRepositoryFake';
+import AppError from '@shared/errs/AppError';
 import CreateAppointmentService from './CreateAppointmentService';
 
 describe('CreateAppointment', () => {
@@ -15,5 +16,26 @@ describe('CreateAppointment', () => {
 
         expect(appointment).toHaveProperty('id');
         expect(appointment.provider_id).toBe('123123');
+    });
+
+    it('should not be able to create two appointments on the same time', async () => {
+        const fakeAppointmentRepository = new AppointmentsRepositoryFake();
+        const createAppointment = new CreateAppointmentService(
+            fakeAppointmentRepository,
+        );
+
+        const appointmentDate = new Date();
+
+        await createAppointment.execute({
+            date: appointmentDate,
+            provider_id: '123123',
+        });
+
+        expect(
+            createAppointment.execute({
+                date: appointmentDate,
+                provider_id: '123123',
+            }),
+        ).rejects.toBeInstanceOf(AppError);
     });
 });
