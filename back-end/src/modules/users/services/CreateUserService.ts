8fcf52feb5452a02c1858d56e189bb44ed2e3c46
Muller Esposito <mulleresposito@hotmail.com>
2021-02-10 23:07:24 -0300

    The create user service has been refactored to use injection and inversion of dependency with HashProvider.

--- back-end/src/modules/users/services/CreateUserService.ts
+++ back-end/src/modules/users/services/CreateUserService.ts
@@ -1,8 +1,7 @@
-import { hash } from 'bcryptjs';
-
 import User from '@modules/users/infra/typeorm/entities/User';
 import AppError from '@shared/errs/AppError';
 import { inject, injectable } from 'tsyringe';
+import IHashProvider from '@modules/users/providers/hashProvider/models/IHashProvider';
 import IUsersRespository from '../infra/repositories/IUsersRepository';
 
 interface IRequest {
@@ -15,6 +14,7 @@ interface IRequest {
 class CreateUserService {
     constructor(
         @inject('UsersRepository') private usersRepository: IUsersRespository,
+        @inject('HashProvider') private hashProvider: IHashProvider,
     ) {}
 
     public async execute({ name, email, password }: IRequest): Promise<User> {
@@ -24,7 +24,7 @@ class CreateUserService {
             throw new AppError('E-mail address already used');
         }
 
-        const hashedPassword = await hash(password, 8);
+        const hashedPassword = await this.hashProvider.generateHash(password);
 
         const user = await this.usersRepository.create({
             name,
