7f7aef486c8bd8d6dfb2da111fd621ea7ccf4370
Muller Esposito <mulleresposito@hotmail.com>
2021-02-10 23:04:39 -0300

    The authenticate user service has been refactored to use injection and inversion of dependency.

--- back-end/src/modules/users/services/AuthenticateUserService.ts
+++ back-end/src/modules/users/services/AuthenticateUserService.ts
@@ -1,10 +1,10 @@
-import { compare } from 'bcryptjs';
 import { sign } from 'jsonwebtoken';
 
 import User from '@modules/users/infra/typeorm/entities/User';
 import authConfig from '@config/auth';
 import AppError from '@shared/errs/AppError';
 import { inject, injectable } from 'tsyringe';
+import IHashProvider from '@modules/users/providers/hashProvider/models/IHashProvider';
 import IUsersRespository from '../infra/repositories/IUsersRepository';
 
 interface IRequest {
@@ -21,6 +21,7 @@ interface IResponse {
 class AutheticationUserService {
     constructor(
         @inject('UsersRepository') private usersRepository: IUsersRespository,
+        @inject('HashProvider') private hashProvider: IHashProvider,
     ) {}
 
     public async execute({ email, password }: IRequest): Promise<IResponse> {
@@ -30,7 +31,10 @@ class AutheticationUserService {
             throw new AppError('Incorrect e-mail/password combination', 401);
         }
 
-        const passwordMatched = await compare(password, user.password);
+        const passwordMatched = await this.hashProvider.compareHash(
+            password,
+            user.password,
+        );
 
         if (!passwordMatched) {
             throw new AppError('Incorrect e-mail/password combination', 401);
